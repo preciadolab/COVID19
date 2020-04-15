d002fa539f51b75750f69fb352f50fea68ff80c3
pacoBarreras <franciscojosebarreras@gmail.com>
2020-04-14 20:11:23 -0400

    Fixed typo with enumerate

--- subset_phila.py
+++ subset_phila.py
@@ -59,7 +59,7 @@ def main():
 
     s_time=time.time()
     print("Starting subset on {} chunks".format(len(file_list)))
-    for file_name in enumerate(file_list):
+    for file_name in file_list:
         cmd='aws s3 cp s3://safegraph-outgoing/movement-sample-global/feb2020/2020/02/'+ k +'/' + file_name + ' ./ --profile safegraph'
         result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         result.check_returncode()
