8779702dd4f9c73ca6cd4b4340db2ca002e1aee0
pacoBarreras <franciscojosebarreras@gmail.com>
2020-05-20 22:03:09 -0400

    implementation of subset_mobility

--- paco_job.sh
+++ paco_job.sh
@@ -2,9 +2,9 @@
 #$ -S /bin/bash      # <- use bash shell
 #$ -N paco_job		 # <- name of job
 #$ -cwd              # <- run job 'here' (required!!)
-#$ -t 1-28           # <- array job first-last
+#$ -t 1-49           # <- array job first-last
 #$ -o output/output_$TASK_ID.out  # <- output file
 #$ -j y              # <- join output & error (best!)
 
-cd /fsx/paco/multiscale_epidemic
-python3 subset_phila.py -d ${SGE_TASK_ID} -m 'Feb'
+cd /fsx/paco/multiscale_epidemic/subsets_and_joins/
+python3 subset_mobility.py -k ${SGE_TASK_ID}
