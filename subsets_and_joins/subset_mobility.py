dcdaca0f0996b94d5305db329e8bdc4df38d3560
pacoBarreras <franciscojosebarreras@gmail.com>
2020-05-20 21:08:38 -0400

    implementation of subset_mobility

--- subsets_and_joins/subset_mobility.py
+++ subsets_and_joins/subset_mobility.py
@@ -77,13 +77,11 @@ def main():
         hashlist_local = df.index.unique()
         df_list = [df.loc[hash_] for hash_ in hashlist if hash_ in hashlist_local]
         
-        pdb.set_trace()        
         df = pd.concat(df_list, ignore_index = True)
-        df.sort_values(by = index, inplace=True)
         df.set_index('index', drop = True, inplace=True)
 
         #subset to hashlist
-        df.to_csv(newfile_path + newfile_name, 'wb+')
+        df.to_csv(newfile_path + newfile_name, 'wb+', index = False)
         #Delete file
         cmd='rm '+file_name
         deleted = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
