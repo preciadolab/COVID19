f12f19a987fe300cd74d55f8d77a8ca720b98b87
pacoBarreras <franciscojosebarreras@gmail.com>
2020-06-18 12:58:08 -0400

    parallel job for visits

--- visits_job.sh
+++ visits_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #$ -S /bin/bash      # <- use bash shell
-#$ -N food_job		 # <- name of job
+#$ -N visits_job		 # <- name of job
 #$ -cwd              # <- run job 'here' (required!!)
-#$ -t 1-67    # <- array job first-last
+#$ -t 16-25,46-55    # <- array job first-last
 #$ -o output/output_$TASK_ID.out  # <- output file
 #$ -j y              # <- join output & error (best!)
 
 cd /fsx/paco/multiscale_epidemic/food_distribution_sites/
-python3 findHome.py -k ${SGE_TASK_ID}
+python3 findVisits.py -k ${SGE_TASK_ID}
